d8edc6151f01f76dccb2e784e02c036470ac18e0
jinHyeokKim <wlsgurrla716@naver.com>
2025-02-13 16:13:07 +0900

    프로젝트 초기 설정

---
+++ DanggeunMarketCloneCoding/DanggeunMarketCloneCoding/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DanggeunMarketCloneCoding
+//
+//  Created by 김진혁 on 2/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DanggeunMarketCloneCoding/DanggeunMarketCloneCoding/DanggeunMarketCloneCodingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DanggeunMarketCloneCodingApp.swift
+//  DanggeunMarketCloneCoding
+//
+//  Created by 김진혁 on 2/13/25.
+//
+
+import SwiftUI
+
+@main
+struct DanggeunMarketCloneCodingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
